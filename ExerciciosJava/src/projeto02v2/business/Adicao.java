928d4fb5b253e62e2602610139e16157f0b8a9f2
Daniel Santana <daniel10gja@gmail.com>
2023-04-22 12:10:56 -0300

    operações

---
+++ ExerciciosJava/src/projeto02v2/business/Adicao.java
@@ -0,0 +1,13 @@
+package projeto02v2.business;
+
+public class Adicao extends Calculo {
+
+    public Adicao(float valor1, float valor2) {
+        super(valor1, valor2);
+    }
+
+    public void calcular() {
+        resultado = valor1 + valor2;
+    }
+
+}
