0be3e473afe180fafd03108d31b48e585c28fd27
Daniel Santana <daniel10gja@gmail.com>
2023-04-28 13:44:10 -0300

    view

--- ExerciciosJava/src/projeto07/view/Principal.java
+++ ExerciciosJava/src/projeto07/view/Principal.java
@@ -1,7 +1,12 @@
 package projeto07.view;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println();
 
     }
 }
