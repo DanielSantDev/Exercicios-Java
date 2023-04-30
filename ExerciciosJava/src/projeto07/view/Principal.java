f58a77926142afb49ca1eb1ae87269bd4e9877c6
Daniel Santana <daniel10gja@gmail.com>
2023-04-30 12:44:57 -0300

    Update Principal.java

--- ExerciciosJava/src/projeto07/view/Principal.java
+++ ExerciciosJava/src/projeto07/view/Principal.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
+        Janela janela = new Janela();
         System.out.println();
 
     }
