ebb8dcee907bf81ab13c0474e2fae5ecf4310d63
Daniel Santana <daniel10gja@gmail.com>
2023-04-30 12:45:00 -0300

    Update Autor.java

--- ExerciciosJava/src/projeto07/business/Autor.java
+++ ExerciciosJava/src/projeto07/business/Autor.java
@@ -3,4 +3,27 @@ package projeto07.business;
 public class Autor {
     String nome, dtNasc, nacionalidade;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDtNasc() {
+        return dtNasc;
+    }
+
+    public void setDtNasc(String dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
 }
