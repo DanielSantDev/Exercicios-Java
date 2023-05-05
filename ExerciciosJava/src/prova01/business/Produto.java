916117ce7a0dd0c3ef365dd3b7c4025e5ee9829b
Daniel Santana <daniel10gja@gmail.com>
2023-05-05 13:42:05 -0300

    Update Produto.java

--- ExerciciosJava/src/prova01/business/Produto.java
+++ ExerciciosJava/src/prova01/business/Produto.java
@@ -1,4 +1,15 @@
 package prova01.business;
 
 public class Produto {
+    String nome, descricao;
+    int codigo;
+    float preco;
+
+    public Produto(String nome, String descricao, int codigo, float preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.codigo = codigo;
+        this.preco = preco;
+    }
+
 }
