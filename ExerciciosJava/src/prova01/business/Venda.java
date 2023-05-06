efa142d6c97868c55d4ea1526583c60f8fefe7fd
Daniel Santana <daniel10gja@gmail.com>
2023-05-06 00:51:57 -0300

    Update Venda.java

--- ExerciciosJava/src/prova01/business/Venda.java
+++ ExerciciosJava/src/prova01/business/Venda.java
@@ -1,4 +1,13 @@
 package prova01.business;
 
 public class Venda {
+    String Cliente;
+    int codigoVenda;
+    float valorTotal;
+
+    public Venda(String cliente, int codigoVenda, float valorTotal) {
+        Cliente = cliente;
+        this.codigoVenda = codigoVenda;
+        this.valorTotal = valorTotal;
+    }
 }
