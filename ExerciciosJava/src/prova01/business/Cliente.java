09c12c9146115394052d65fb1bc8f53777a616c9
Daniel Santana <daniel10gja@gmail.com>
2023-05-06 00:51:54 -0300

    Update Cliente.java

--- ExerciciosJava/src/prova01/business/Cliente.java
+++ ExerciciosJava/src/prova01/business/Cliente.java
@@ -1,4 +1,11 @@
 package prova01.business;
 
 public class Cliente {
+    String nome, CPF, endereco;
+
+    public Cliente(String nome, String CPF, String endereco) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.endereco = endereco;
+    }
 }
